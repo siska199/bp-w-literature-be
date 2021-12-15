cdc932c5c7a81c31f4c09501802b21c3dbf6dd60
siska199 <siska.apriana.1703226@students.um.ac.id>
2021-12-15 09:06:31 +0700

    updated backend

---
+++ seeders/20211215015724-user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+     await queryInterface.bulkInsert(
+       'users',
+      [{
+          fullName: 'Siska Apriana Rifianti',
+          email: 'admin@gmail.com',
+          password:
+            '$2b$10$.c5VhzZuRINU566qjijggOp.Q/NFjyP51zjwyNyk8DzHXc8V9uL6C', //11aprilnaruto
+          phone : '123456789',
+          address: 'Jl. Gunung Kawi',
+          status: 'admin',
+          gender: 'Female',
+          image: 'https://res.cloudinary.com/university-state-of-malang-city/image/upload/v1639533924/avatar/male6_dx9ywz.png'
+      }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
