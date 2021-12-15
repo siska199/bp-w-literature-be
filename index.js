9f6e914bd0d9d601c25f3e77ca03bb241f69741a
siska199 <siska.apriana.1703226@students.um.ac.id>
2021-12-15 16:18:04 +0700

    updated backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors: {
-        origin : 'http://localhost:3000'
+        origin : 'https://literature199.netlify.app/'
     }
 })
 require('./src/socket')(io)
